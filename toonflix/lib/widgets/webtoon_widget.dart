0b79bff1e75b88f4f488985d48fd279d2dee4d76
junil.oh <junil.oh@junilohcBookPro.kornet>
2023-06-19 20:48:00 +0900

    #6.10 hero

--- toonflix/lib/widgets/webtoon_widget.dart
+++ toonflix/lib/widgets/webtoon_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/detail_screen.dart';
-import '../models/webtoon_model.dart';
+//import '../models/webtoon_model.dart';
 
 class Webtoon extends StatelessWidget {
   const Webtoon({
@@ -30,20 +30,23 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 7,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ]),
-            child: Image.network(
-              thumb,
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 7,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ]),
+              child: Image.network(
+                thumb,
+              ),
             ),
           ),
           const SizedBox(
